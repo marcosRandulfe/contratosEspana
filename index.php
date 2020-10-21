b8037be59d62d9e3ee3cf9bebdff93313accef41
marcosrgdev <marcosrandulfegarrido@gmail.com>
2020-10-21 13:42:55 +0200

    [TEST] gecko marionete

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 
+    //shell_exec('/home/marcos/geckodriver');
+
     require './vendor/autoload.php';
     use Facebook\WebDriver\Remote\RemoteWebDriver;
     use Facebook\WebDriver\Remote\DesiredCapabilities;
     use Facebook\WebDriver\WebDriverBy;
+    use Facebook\WebDriver\WebDriverExpectedCondition;
 
     // Geckodriver
     $host = 'http://localhost:4444';
@@ -36,15 +39,17 @@
     $elemento=$driver->findElement(WebDriverBy::xpath("//input[@value='Buscar']"));
     $elemento->click();
 
-    $driver->wait()->until($driver->findElement(WebDriverBy::id("myTablaBusquedaCustom")));
+    //$elemento=$driver->findElement(WebDriverBy::id("myTablaBusquedaCustom"));
+    var_dump(WebDriverBy::id('myTablaBusquedaCustom'));
+    $driver->wait(10,100)->until(WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::id('myTablaBusquedaCustom')));
     $elementos=$driver->findElements(WebDriverBy::xpath("//table[@id='myTablaBusquedaCustom']/tbody/tr/td[1]//a"));
-    var_dump($elementos);
-
-    $elemento->click();
-    
-    
-
-
-
-
+    $num_elementos = (count($elementos));
+    $num_elemento=0;
+    //$elementos[0]->click();
+    while($num_elemento<$num_elementos){
+        $elementos[$num_elemento]->click();
+        $driver->
+        $num_elemento++;
+        break;
+    }
 ?>
